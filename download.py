f3fdcffcbbf7660d113a60b6f7abcbe2938c222b
EliasPeeters <Elias.peeters@me.com>
2020-12-08 09:52:26 +0100

    Added download

--- download.py
+++ download.py
@@ -1,6 +1,7 @@
 import requests
 import json
 import os
+import csv
 
 def getToken():
     data = {'username': os.environ['OK_USERNAME'], 'password': os.environ['OK_PASSWORD'], 'grant_type': 'password', 'client_id': os.environ['OK_CLIENT_ID']}
@@ -64,7 +65,38 @@ def writeCategories(input):
 
     writeFile(outputString, 'categories.js')
 
+def createCSV(data, filename):
+
+    with open('csv/' + str(filename) + '.csv', 'w', newline='') as csvfile:
+        fieldnames = ['name', 'attributeGroup', 'type', 'multiValue', 'featureRelevance', 'relevance', 'description', 'exampleValues', 'allowedValues', 'recommendedValues', 'reference']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        
+        writer.writeheader()
+        for x in data['attributes']:
+            row = {}
+            row['name'] = x['name']
+            row['attributeGroup'] = x['attributeGroup']
+            row['type'] = x['type']
+            row['multiValue'] = x['multiValue']
+            row['featureRelevance'] = ', '.join(x['featureRelevance'])
+            row['relevance'] = x['relevance']
+            row['description'] = x['description']
+
+            if 'exampleValues' in x:
+               row['exampleValues'] = ', '.join(x['exampleValues'])
+
+            if 'allowedValues' in x:
+                row['allowedValues'] = ', '.join(x['allowedValues'])
+            
+            if 'recommendedValues' in x:
+                row['recommendedValues'] = ', '.join(x['recommendedValues'])
+
+            row['reference'] = x['reference']
+            writer.writerow(row)
+        
+
 def storeFiles(data, fileName):
+    createCSV(data, fileName)
     header = ''
     with open('jsons/' + str(fileName)+ '.json', 'w') as outfile:
         json.dump(data, outfile)
@@ -78,6 +110,8 @@ index = createIndexing(categories)
 writeIndex(index)
 for x in range(len(categories['categoryGroups'])):
     storeFiles(categories['categoryGroups'][x], x)
+
+createCSV(categories['categoryGroups'][x], x)
 #storeJSON(categories['categoryGroups'][0])
 #writeFile(categories['categoryGroups'][0], '0.json')
-#writeCategories(categories['categoryGroups'])
+#writeCategories(categories['categoryGroups'])  
